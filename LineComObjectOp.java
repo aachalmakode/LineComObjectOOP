365b8ff1fca6106d435917e8df82e8f098bc3ce2
aachalmakode <aachalmakode900@gmail.com>
2021-06-22 20:24:58 -0400

    Wecome to program

---
+++ LineComObjectOp.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class LineComparisonOp {
+    public static void main(String[] args) {
+        System.out.println( "Welcome to Line Comparison Computation OOP Program" );
+    }
+}
+
